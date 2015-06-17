b59aae8fd7450c8d8858ec332448b9fc089db365
Josh <joshualherndon@gmail.com>
2015-06-16 23:29:35 -0500

    Started porting over the panel code- everything is here but it doesn't seem to play well with cartodb

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,5 @@
 var map;
+var currentParcelData;
 
 function main() {
 
@@ -6,7 +7,8 @@ function main() {
     center: [39.08, -94.55],
     zoom: 14, 
     zoomControl: false,  // dont add the zoom overlay (it is added by default)
-    loaderControl: false //dont show tiles loader
+    loaderControl: false, //dont show tiles loader
+    query: 'SELECT * FROM data'
 
   };
   
@@ -52,16 +54,28 @@ function main() {
         })
 */
 
-        var layer = 'https://code4kc.cartodb.com/api/v2_1/viz/8167c2b8-0cf3-11e5-8080-0e9d821ea90d/viz.json';
+        var datalayer = 'https://code4kc.cartodb.com/api/v2_1/viz/8167c2b8-0cf3-11e5-8080-0e9d821ea90d/viz.json';
 
-        cartodb.createLayer(map, layer).addTo(map).on('done', function(layer){
+        cartodb.createLayer(map, datalayer).addTo(map).on('done', function(layer){
           var sublayer = layer.getSubLayer(2); //sublayer generated from the data.json file
-
           sublayer.infowindow.set('template', $('#infowindow_template').html());
+          sublayer.setInteraction(true);
+          //add more data as needed:
+          sublayer.setInteractivity('cartodb_id, address, apn, kivapin, land_ban30, land_ban_6, land_ban_4, land_ban_6, land_ban_7, land_ban10, land_ban36, land_ban56, land_ban60, land_bank_, own_name');
+          sublayer.on('featureClick', function(e, latlng, pos, data, layer) {
+            currentParcelData = data;
+          });
         }).on('error', function(){
-          console.log("error loading layer info");
+          console.log("Error");
         });
 
+        
+  $('.cd-panel').on('click', function(event){
+    if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+      $('.cd-panel').removeClass('is-visible');
+      event.preventDefault();
+    }
+  });
 //        q.await(function() {
 //          var leafletLayers = Array.prototype.slice.call(arguments, 1);
 //          leafletLayers.forEach(function(lyr) {
@@ -71,4 +85,21 @@ function main() {
 
 }
 
+function openPanel(event){
+  $('.cd-panel').addClass('is-visible');
+  console.log("Hello");
+}
+
 window.onload = main;
+
+//panel related code
+jQuery(document).ready(function($){
+
+  $('.cd-panel').on('click', function(event){
+    if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+      $('.cd-panel').removeClass('is-visible');
+      event.preventDefault();
+    }
+  });
+});
+
