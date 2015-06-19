aedaa9731b4743232bf7ca29fc1bd758c791609c
Josh <joshualherndon@gmail.com>
2015-06-18 23:11:09 -0500

    Recreated most of the mockup and filled in as much as I could with the data from the geojson

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,4 @@
 var map;
-var currentParcelData;
 
 function main(){
 
@@ -27,9 +26,9 @@ function main(){
     sublayer.infowindow.set('template', $('#infowindow_template').html());
     sublayer.setInteraction(true);
     //add more data as needed:
-    sublayer.setInteractivity('cartodb_id, address, apn, kivapin, land_ban30, land_ban_6, land_ban_4, land_ban_6, land_ban_7, land_ban10, land_ban36, land_ban56, land_ban60, land_bank_, own_name');
+    sublayer.setInteractivity('cartodb_id, address, apn, kivapin, land_ban30, land_ban_6, land_ban_4, land_ban_6, land_ban_7, land_ban10, land_ban36, land_ban56, land_ban60, land_bank_, own_name, landusecod, land_ban32, land_ban_3');
     sublayer.on('featureClick', function(e, latlng, pos, data, layer) {
-      currentParcelData = data;
+      populatePanel(data);
 
       $('.cartodb-infowindow').on('click', '#openpanel' ,function(){
         $('.cd-panel').addClass('is-visible');
@@ -38,6 +37,74 @@ function main(){
   }).on('error', function(){
     console.log("Error");
   });
+}
+
+function populatePanel(data){
+  $('#AddressTitle').text(data.land_ban60);
+
+  //build the general tab
+  var template = $('#generalbox_template').html();
+  var rendered = Mustache.render(template, {
+      owner: data.own_name, 
+      landuse: data.land_bank_, 
+      landusecode: data.landusecod,
+      sqrft: data.land_ban30,
+      council: data.land_ban_7,
+      school: data.land_ban10,
+      neighborhood: data.land_ban_6,
+      bff: "n/a",
+      date: "n/a",
+      assland: "n/a",
+      assimprove: "n/a",
+      exland: "n/a",
+      eximprove: "n/a",
+      acres: "n/a",
+      perimeter: "n/a",
+      plss: "n/a"
+
+  });
+  $('#general').html(rendered);
+
+  //build the tax tab
+  template = $('#taxbox_template').html();
+  var rendered = Mustache.render(template, {
+    assessedvalue: "n/a",
+    taxvalue: "n/a",
+    levy: "n/a",
+    prevyear: "2014",
+    prevtax: "n/a",
+    lastassessed: "n/a"
+  });
+  $('#tax').html(rendered);
+
+  //build the neighborhood tab
+  template = $('#nhoodbox_template').html();
+  rendered = Mustache.render(template);
+  $('#neighborhood').html(rendered);
+
+  //build the plan tab
+  template = $('#planbox_template').html();
+  rendered = Mustache.render(template);
+  $('#plan').html(rendered);
+
+  //build the incentives tab
+  template = $('#incentivesbox_template').html();
+  rendered = Mustache.render(template);
+  $('#incentives').html(rendered);
+
+  //build the links tab
+  template = $('#linksbox_template').html();
+  rendered = Mustache.render(template, {
+    kivapin: data.kivapin
+  });
+  $('#links').html(rendered);
+
+  //populate the zoning select
+  var zoningselect = $('#ZoningSelect');
+  zoningselect.append($("<option \>").val(data.land_ban_3).text(data.land_ban_3));
+
+  //TODO: fill in the other select boxes, zoning select should probably have more options, and the fill in the building evelope
+
 }
 
 
